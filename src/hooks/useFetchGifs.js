6a998014cd6920facc39768be1bc061cd1d0b149
Damian Cachagua <d_cachagua@hotmail.com.ar>
2021-09-22 22:21:54 -0300

    prueba

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useEffect } from 'react/cjs/react.development';
+//import { useEffect } from 'react/cjs/react.development';
 import { getGifts } from '../helpers/getGifs';
 
 export const useFetchGifs = ( category ) => {
